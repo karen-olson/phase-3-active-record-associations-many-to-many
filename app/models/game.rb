7c5e47d78c4b20a50bfd94de831f3fd8efcd3408
Karen Olson <karmarie@gmail.com>
2021-10-21 13:41:49 -0500

    commit tree 0addb9324d0ffad18dac48d9c97133fd7c27a7d0

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
+  # self.has_many(:reviews)
+  # self.has_many(:users, through: :reviews)
   has_many :reviews
+  has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  
+#   self.has_many(:reviews)
+  has_many :reviews
+#   self.has_many(:games, through: :reviews)
+  has_many :games, through: :reviews
 end
